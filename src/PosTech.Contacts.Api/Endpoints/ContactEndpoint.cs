cef20c0efe979cc4a992f80f10aa2d5ed8f0ba14
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-01-14 15:14:43 -0300

    Separar método dos endpoints para melhorar a legibilidade da classe

---
+++ src/PosTech.Contacts.Api/Endpoints/ContactEndpoint.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using PosTech.Contacts.ApplicationCore.Commands;
+using PosTech.Contacts.ApplicationCore.DTOs.Requests;
+
+namespace PosTech.Contacts.Api.Endpoints
+{
+    public static class ContactEndpoint
+    {
+        public static async Task<IResult> CreateContactAsync(IMediator mediator, IValidator<CreateAndUpdateContactRequestDto> validator,
+            IMapper mapper, CreateAndUpdateContactRequestDto createContactRequestDto)
+        {
+            var validationResult = await validator.ValidateAsync(createContactRequestDto);
+            if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var createContactCommand = mapper.Map<CreateContactCommand>(createContactRequestDto);
+
+            await mediator.Send(createContactCommand);
+
+            return TypedResults.Created();
+        }
+
+        public static async Task<IResult> SearchContactsAsync(IMediator mediator, IMapper mapper,
+            IValidator<SearchContactRequestDto> validator, SearchContactRequestDto searchContactRequestDto)
+        {
+            var validationResult = await validator.ValidateAsync(searchContactRequestDto);
+            if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var searchContactCommand = mapper.Map<SearchContactsCommand>(searchContactRequestDto);
+            var contacts = await mediator.Send(searchContactCommand);
+
+            if (contacts is null || contacts.Count == 0) return Results.NotFound();
+
+            return Results.Ok(contacts);
+        }
+
+        public static async Task<IResult> GetContactByIdAsync(IMediator mediator, Guid id)
+        {
+            var contact = await mediator.Send(new GetContactByIdCommand { Id = id });
+
+            if (contact is null) return Results.NotFound();
+
+            return Results.Ok(contact);
+        }
+
+        public static async Task<IResult> UpdateContactAsync(IMediator mediator, IValidator<CreateAndUpdateContactRequestDto> validator, 
+            IMapper mapper, CreateAndUpdateContactRequestDto updateContactRequestDto, Guid id)
+        {
+            var validationResult = await validator.ValidateAsync(updateContactRequestDto);
+            if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
+
+            var updateContactCommand = mapper.Map<UpdateContactCommand>(updateContactRequestDto);
+            updateContactCommand.Id = id;
+
+            await mediator.Send(updateContactCommand);
+
+            return Results.Ok();
+        }
+
+        public static async Task<IResult> DeleteContactAsync(IMediator mediator, Guid id)
+        {
+            await mediator.Send(new DeleteContactCommand { Id = id });
+
+            return Results.Ok();
+        }
+    }
+}
--- src/PosTech.Contacts.Api/Program.cs
+++ src/PosTech.Contacts.Api/Program.cs
@@ -1,11 +1,10 @@
 using AutoMapper;
 using FluentValidation;
 using MediatR;
+using PosTech.Contacts.Api.Endpoints;
 using PosTech.Contacts.ApplicationCore;
-using PosTech.Contacts.ApplicationCore.Commands;
 using PosTech.Contacts.ApplicationCore.Constants;
 using PosTech.Contacts.ApplicationCore.DTOs.Requests;
-using PosTech.Contacts.ApplicationCore.DTOs.Responses;
 using PosTech.Contacts.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,83 +25,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+#region Contact Endpoits
 
 RouteGroupBuilder contacts = app.MapGroup(RouteConst.Contacts);
 
 contacts.MapPost("/", async (IMediator mediator, IMapper mapper,
     IValidator<CreateAndUpdateContactRequestDto> validator, CreateAndUpdateContactRequestDto contactDto)
-    => await CreateContactAsync(mediator, validator, mapper, contactDto));
+    => await ContactEndpoint.CreateContactAsync(mediator, validator, mapper, contactDto));
 
 contacts.MapPost("/search", async (IMediator mediator, IMapper mapper,
     IValidator<SearchContactRequestDto> validator, SearchContactRequestDto searchContactRequestDto)
-    => await SearchContactsAsync(mediator, mapper, validator, searchContactRequestDto));
+    => await ContactEndpoint.SearchContactsAsync(mediator, mapper, validator, searchContactRequestDto)).CacheOutput();
 
 contacts.MapGet("/{id:guid}", async (IMediator mediator, Guid id)
-    => await GetContactByIdAsync(mediator, id));
+    => await ContactEndpoint.GetContactByIdAsync(mediator, id)).CacheOutput();
 
 contacts.MapPut("/{id:guid}", async (IMediator mediator, IMapper mapper,
     IValidator<CreateAndUpdateContactRequestDto> validator, CreateAndUpdateContactRequestDto contactDto, Guid id)
-    => await UpdateContactAsync(mediator, validator, mapper, contactDto, id));
+    => await ContactEndpoint.UpdateContactAsync(mediator, validator, mapper, contactDto, id));
 
 contacts.MapDelete("/{id:guid}", async (IMediator mediator, Guid id)
-    => await DeleteContactAsync(mediator, id));
+    => await ContactEndpoint.DeleteContactAsync(mediator, id));
 
-static async Task<IResult> CreateContactAsync(IMediator mediator, IValidator<CreateAndUpdateContactRequestDto> validator, IMapper mapper,
-    CreateAndUpdateContactRequestDto createContactRequestDto)
-{
-    var validationResult = await validator.ValidateAsync(createContactRequestDto);
-    if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
-
-    var createContactCommand = mapper.Map<CreateContactCommand>(createContactRequestDto);
-
-    await mediator.Send(createContactCommand);
-
-    return TypedResults.Created();
-}
-
-static async Task<IResult> SearchContactsAsync(IMediator mediator, IMapper mapper,
-    IValidator<SearchContactRequestDto> validator, SearchContactRequestDto searchContactRequestDto)
-{
-    var validationResult = await validator.ValidateAsync(searchContactRequestDto);
-    if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
-
-    var searchContactCommand = mapper.Map<SearchContactsCommand>(searchContactRequestDto);
-    var contacts = await mediator.Send(searchContactCommand);
-
-    if (contacts is null || contacts.Count == 0) return Results.NotFound();
-
-    return Results.Ok(contacts);
-}
-
-static async Task<IResult> GetContactByIdAsync(IMediator mediator, Guid id)
-{
-    var contact = await mediator.Send(new GetContactByIdCommand { Id = id });
-
-    if (contact is null) return Results.NotFound();
-
-    return Results.Ok(contact);
-}
-static async Task<IResult> UpdateContactAsync(IMediator mediator, IValidator<CreateAndUpdateContactRequestDto> validator, IMapper mapper,
-    CreateAndUpdateContactRequestDto updateContactRequestDto, Guid id)
-{
-    var validationResult = await validator.ValidateAsync(updateContactRequestDto);
-    if (!validationResult.IsValid) return Results.ValidationProblem(validationResult.ToDictionary());
-
-    var updateContactCommand = mapper.Map<UpdateContactCommand>(updateContactRequestDto);
-    updateContactCommand.Id = id;
-
-    await mediator.Send(updateContactCommand);
-
-    return Results.Ok();
-}
-
-static async Task<IResult> DeleteContactAsync(IMediator mediator, Guid id)
-{
-    await mediator.Send(new DeleteContactCommand { Id = id });
-
-    return Results.Ok();
-}
+#endregion
 
 app.Run();
