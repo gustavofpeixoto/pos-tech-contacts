b41737e348c9dd1a0fddbeb0bf91b279da56698b
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-01-15 10:41:22 -0300

    Incluir o cache no endpoint de busca de contato por id

--- PosTech.Contacts.Domain/Handlers/GetContactByIdCommandHandler.cs
+++ PosTech.Contacts.Domain/Handlers/GetContactByIdCommandHandler.cs
@@ -3,20 +3,28 @@ using MediatR;
 using PosTech.Contacts.ApplicationCore.Commands;
 using PosTech.Contacts.ApplicationCore.DTOs.Responses;
 using PosTech.Contacts.ApplicationCore.Repositories;
+using PosTech.Contacts.ApplicationCore.Services;
 
 namespace PosTech.Contacts.ApplicationCore.Handlers
 {
-    public class GetContactByIdCommandHandler(IMapper mapper, IContactRepository contactRepository) 
+    public class GetContactByIdCommandHandler(ICacheService cacheService, IContactRepository contactRepository, IMapper mapper)
         : IRequestHandler<GetContactByIdCommand, ContactResponseDto>
     {
-        private readonly IMapper _mapper = mapper;
+        private readonly ICacheService _cacheService = cacheService;
         private readonly IContactRepository _contactRepository = contactRepository;
+        private readonly IMapper _mapper = mapper;
 
         public async Task<ContactResponseDto> Handle(GetContactByIdCommand request, CancellationToken cancellationToken)
         {
+            var key = request.Id.ToString();
+            if (_cacheService.TryGetValue<ContactResponseDto>(key, out var contact))
+                return contact;
+
             var storageContact = await _contactRepository.GetByIdAsync(request.Id);
 
-            var contact = _mapper.Map<ContactResponseDto>(storageContact);
+            contact = _mapper.Map<ContactResponseDto>(storageContact);
+
+            await _cacheService.SetAsync<ContactResponseDto>(key, contact, TimeSpan.FromMinutes(10));
 
             return contact;
         }
