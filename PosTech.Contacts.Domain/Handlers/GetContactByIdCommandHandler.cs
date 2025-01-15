348d48af8b7433e220534b554a6eb8ad33807ad7
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-01-15 12:06:52 -0300

    Incluir log no endpoint de busca por id.

--- PosTech.Contacts.Domain/Handlers/GetContactByIdCommandHandler.cs
+++ PosTech.Contacts.Domain/Handlers/GetContactByIdCommandHandler.cs
@@ -4,6 +4,7 @@ using PosTech.Contacts.ApplicationCore.Commands;
 using PosTech.Contacts.ApplicationCore.DTOs.Responses;
 using PosTech.Contacts.ApplicationCore.Repositories;
 using PosTech.Contacts.ApplicationCore.Services;
+using Serilog;
 
 namespace PosTech.Contacts.ApplicationCore.Handlers
 {
@@ -16,15 +17,26 @@ namespace PosTech.Contacts.ApplicationCore.Handlers
 
         public async Task<ContactResponseDto> Handle(GetContactByIdCommand request, CancellationToken cancellationToken)
         {
+            Log.Information("Iniciando busca do contato com Id: {contactId}", request.Id);
+
             var key = request.Id.ToString();
+
             if (_cacheService.TryGetValue<ContactResponseDto>(key, out var contact))
+            {
+                Log.Information("Retornando contato armazenado no cache. Id: {contactId}", request.Id);
+
                 return contact;
+            }
 
             var storageContact = await _contactRepository.GetByIdAsync(request.Id);
 
             contact = _mapper.Map<ContactResponseDto>(storageContact);
 
-            await _cacheService.SetAsync<ContactResponseDto>(key, contact, TimeSpan.FromMinutes(10));
+            Log.Information("Armazenando contatos no cache. Id: {contactId}", request.Id);
+
+            await _cacheService.SetAsync(key, contact, TimeSpan.FromMinutes(10));
+
+            Log.Information("Finalizando busca do contato com Id: {contactId}", request.Id);
 
             return contact;
         }
