546d85a4299c5458a41a97d1a22ccd50d0e19078
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-03-11 16:50:58 -0300

    Incluir suporte ao docker. Habilitar swagger na versão publicada

--- src/PosTech.Contacts.Api/Program.cs
+++ src/PosTech.Contacts.Api/Program.cs
@@ -25,12 +25,8 @@ builder.Services.AddApplicationCoreServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
