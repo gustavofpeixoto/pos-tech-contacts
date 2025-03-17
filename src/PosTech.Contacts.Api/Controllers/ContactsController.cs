5a961efee5eb392c7e279ed7e119a9879abcc206
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-03-17 18:22:31 -0300

    Criar controller de contacts

---
+++ src/PosTech.Contacts.Api/Controllers/ContactsController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PosTech.Contacts.ApplicationCore.Commands;
+using PosTech.Contacts.ApplicationCore.DTOs.Requests;
+using PosTech.Contacts.ApplicationCore.DTOs.Responses;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace PosTech.Contacts.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController(IMediator mediator, IMapper mapper) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+        private readonly IMapper _mapper = mapper;
+
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ContactResponseDto))]
+        public async Task<IActionResult> CreateContactAsync(CreateAndUpdateContactRequestDto createContactRequestDto)
+        {
+            var createContactCommand = _mapper.Map<CreateContactCommand>(createContactRequestDto);
+            var result = await _mediator.Send(createContactCommand);
+
+            return Ok(result);
+        }
+
+        [HttpPost("/search")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(List<ContactResponseDto>))]
+        [SwaggerResponse(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> SearchContactsAsync(SearchContactRequestDto searchContactRequestDto)
+        {
+            var searchContactCommand = _mapper.Map<SearchContactsCommand>(searchContactRequestDto);
+            var contacts = await _mediator.Send(searchContactCommand);
+
+            if (contacts is null || contacts.Count == 0) return NoContent();
+
+            return Ok(contacts);
+        }
+
+        [HttpGet("{id:guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ContactResponseDto))]
+        [SwaggerResponse(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> GetContactByIdAsync(Guid id)
+        {
+            var contact = await _mediator.Send(new GetContactByIdCommand { Id = id });
+
+            if (contact is null) return NoContent();
+
+            return Ok(contact);
+        }
+
+        [HttpPut("{id:guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ContactResponseDto))]
+        [SwaggerResponse(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> UpdateContactAsync(CreateAndUpdateContactRequestDto updateContactRequestDto, Guid id)
+        {
+            var updateContactCommand = _mapper.Map<UpdateContactCommand>(updateContactRequestDto);
+            updateContactCommand.Id = id;
+
+            var result = await _mediator.Send(updateContactCommand);
+
+            if (result is null) return UnprocessableEntity();
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(ContactResponseDto))]
+        public async Task<IActionResult> DeleteContactAsync(Guid id)
+        {
+            await _mediator.Send(new DeleteContactCommand { Id = id });
+
+            return Ok();
+        }
+    }
+}
--- src/PosTech.Contacts.Api/Program.cs
+++ src/PosTech.Contacts.Api/Program.cs
@@ -1,86 +1,58 @@
-using AutoMapper;
 using FluentValidation;
+using FluentValidation.AspNetCore;
 using MediatR;
-using PosTech.Contacts.Api.Endpoints;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using PosTech.Contacts.Api.Settings;
 using PosTech.Contacts.ApplicationCore;
-using PosTech.Contacts.ApplicationCore.Constants;
-using PosTech.Contacts.ApplicationCore.DTOs.Requests;
-using PosTech.Contacts.ApplicationCore.DTOs.Responses;
+using PosTech.Contacts.ApplicationCore.Validators;
 using PosTech.Contacts.Infrastructure;
 using Prometheus;
 using Prometheus.DotNetRuntime;
 using Serilog;
+using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-var prometheusBuilder = DotNetRuntimeStatsBuilder.Default();
-prometheusBuilder = DotNetRuntimeStatsBuilder.Customize()
-    .WithContentionStats(CaptureLevel.Informational)
-    .WithGcStats(CaptureLevel.Verbose)
-    .WithThreadPoolStats(CaptureLevel.Informational)
-    .WithExceptionStats(CaptureLevel.Errors)
-    .WithJitStats();
+        var prometheusBuilder = DotNetRuntimeStatsBuilder.Default();
+        prometheusBuilder = DotNetRuntimeStatsBuilder.Customize()
+            .WithContentionStats(CaptureLevel.Informational)
+            .WithGcStats(CaptureLevel.Verbose)
+            .WithThreadPoolStats(CaptureLevel.Informational)
+            .WithExceptionStats(CaptureLevel.Errors)
+            .WithJitStats();
 
-prometheusBuilder.RecycleCollectorsEvery(new TimeSpan(0, 20, 0));
+        prometheusBuilder.RecycleCollectorsEvery(new TimeSpan(0, 20, 0));
+        prometheusBuilder.StartCollecting();
 
-prometheusBuilder.StartCollecting();
+        builder.Services
+            .AddControllers(options
+                => options.Conventions.Add(new RouteTokenTransformerConvention(new OutboundParameterTransformerSetting())))
+            .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+        builder.Services.AddSerilog();
+        builder.Services.AddValidatorsFromAssemblyContaining<CreateAndUpdateContactValidator>();
+        builder.Services.AddFluentValidationAutoValidation();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+        builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+        builder.Services.AddApplicationCoreServices();
+        builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddControllers();
-builder.Services.AddSerilog();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
-builder.Services.AddApplicationCoreServices();
+        var app = builder.Build();
 
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-app.UseHttpMetrics();
-app.UseMetricServer();
-
-#region Contact Endpoits
-
-RouteGroupBuilder contacts = app.MapGroup(RouteConst.Contacts);
-
-contacts.MapPost("/", async (IMediator mediator, IMapper mapper,
-    IValidator<CreateAndUpdateContactRequestDto> validator, CreateAndUpdateContactRequestDto contactDto)
-    => await ContactEndpoint.CreateContactAsync(mediator, validator, mapper, contactDto))
-    .WithOpenApi(op => new(op) { Description = "Endpoint para cadastro de novos contatos." })
-    .Produces<ContactResponseDto>(StatusCodes.Status200OK);
-
-contacts.MapPost("/search", async (IMediator mediator, IMapper mapper,
-    IValidator<SearchContactRequestDto> validator, SearchContactRequestDto searchContactRequestDto)
-    => await ContactEndpoint.SearchContactsAsync(mediator, mapper, validator, searchContactRequestDto))
-    .WithOpenApi(op => new(op) { Description = @"Endpoint de busca avançada. 
-Todos os parâmetros submetidos serão utilizados para filtrar os contatos do banco de dados." })
-    .Produces<List<ContactResponseDto>>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status204NoContent);
-
-contacts.MapGet("/{id:guid}", async (IMediator mediator, Guid id)
-    => await ContactEndpoint.GetContactByIdAsync(mediator, id)).CacheOutput()
-    .WithOpenApi(op => new(op) { Description = "Endpoint para busca de contatos por identificador único." })
-    .Produces<ContactResponseDto>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status204NoContent);
-
-contacts.MapPatch("/{id:guid}", async (IMediator mediator, IMapper mapper,
-    IValidator<CreateAndUpdateContactRequestDto> validator, CreateAndUpdateContactRequestDto contactDto, Guid id)
-    => await ContactEndpoint.UpdateContactAsync(mediator, validator, mapper, contactDto, id))
-    .WithOpenApi(op => new(op) { Description = "Entpoint para atualizar as informaões do contato." })
-    .Produces<ContactResponseDto>(StatusCodes.Status200OK)
-    .Produces(StatusCodes.Status422UnprocessableEntity);
-
-contacts.MapDelete("/{id:guid}", async (IMediator mediator, Guid id)
-    => await ContactEndpoint.DeleteContactAsync(mediator, id))
-    .WithOpenApi(op => new(op) { Description = "Remove completamente as informações do contato da base de dados." })
-    .Produces(StatusCodes.Status200OK);
-
-#endregion
-
-app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseHttpMetrics();
+        app.UseMetricServer();
+        app.MapControllers();
+        app.Run();
+    }
+}
