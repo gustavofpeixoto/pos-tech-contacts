c5f3ba76ca04a02186f45e7ccb7db4e117f85089
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-05-05 08:46:47 -0300

    Criar produtor de mensages

---
+++ src/PosTech.Contacts.Infrastructure/Messaging/RabbitMqMessagingProducer.cs
@@ -0,0 +1,33 @@
+﻿using PosTech.Contacts.ApplicationCore.Messaging;
+using PosTech.Contacts.ApplicationCore.Serialization;
+using PosTech.Contacts.Infrastructure.Settings;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace PosTech.Contacts.Infrastructure.Messaging
+{
+    public class RabbitMqMessagingProducer(RabbitMqSettings settings) : IMessagingProducer
+    {
+        public async Task SendAsync<TMessage>(TMessage message, string queue)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = settings.HostName,
+                VirtualHost = settings.VirtualHost,
+                UserName = settings.UserName,
+                Password = settings.Password,
+            };
+
+            var connection = await connectionFactory.CreateConnectionAsync();
+
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue, exclusive: false);
+
+            var serializedMessage = JsonSerializerHelper.Serialize(message);
+            var messageBody = Encoding.UTF8.GetBytes(serializedMessage);
+
+            await channel.BasicPublishAsync(exchange: "", routingKey: queue, body: messageBody);
+        }
+    }
+}
