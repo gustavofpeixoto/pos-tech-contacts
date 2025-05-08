b07119fdfd25c47fb88f7d404c4bd660461458f6
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-05-08 09:11:10 -0300

    Adicionar entidade do seguimento query do CQRS

---
+++ src/PosTech.Contacts.ApplicationCore/Entities/Query/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace PosTech.Contacts.ApplicationCore.Entities.Query
+{
+    public class Contact(Guid id)
+    {
+        public Guid Id { get; private set; } = id;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int DddCode { get; set; }
+        public string DddState { get; set; }
+        public string RegionName { get; set; }
+    }
+}
