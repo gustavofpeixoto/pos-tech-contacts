93077b2433fe5fbd470bba28afd4b40ad7934693
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-05-12 17:28:47 -0300

    Incluir consumidor para mensagem de contato excluído

---
+++ src/PosTech.Contacts.Worker/Consumers/ContactDeletedMessageConsumer.cs
@@ -0,0 +1,39 @@
+﻿using PosTech.Contacts.ApplicationCore.Messaging;
+using PosTech.Contacts.ApplicationCore.Repositories.Query;
+using PosTech.Contacts.ApplicationCore.Serialization;
+using PosTech.Contacts.Infrastructure.Settings;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace PosTech.Contacts.Worker.Consumers
+{
+    public class ContactDeletedMessageConsumer(IContactRepository contactRepository, RabbitMqSettings settings)
+        : RabbitMqConsumer(settings)
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await ConnectAsync(QueueNames.ContactDeleted, stoppingToken);
+
+            var consumer = new AsyncEventingBasicConsumer(Channel);
+            consumer.ReceivedAsync += ProcessMessageAsync;
+
+            await Channel.BasicConsumeAsync(QueueNames.ContactDeleted, autoAck: true, consumer: consumer, cancellationToken: stoppingToken);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine(
+                   $"Worker ativo em: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            }
+        }
+
+        protected override async Task ProcessMessageAsync(object sender, BasicDeliverEventArgs ea)
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var deserializedMessage = JsonSerializerHelper.Deserialize<ContactDeletedMessage>(message);
+
+            await contactRepository.DeleteAsync(deserializedMessage.ContactId);
+        }
+    }
+}
