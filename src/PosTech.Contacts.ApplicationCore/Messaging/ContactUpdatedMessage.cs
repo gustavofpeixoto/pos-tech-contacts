15018dc43b2e0b477ef5eea65bfd174204b7f07e
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-05-12 17:21:43 -0300

    Adicionar consumidor para mensagem de contato atualizado

--- src/PosTech.Contacts.ApplicationCore/Messaging/ContactUpdatedMessage.cs
+++ src/PosTech.Contacts.ApplicationCore/Messaging/ContactUpdatedMessage.cs
@@ -1,6 +1,4 @@
-﻿using PosTech.Contacts.ApplicationCore.Entities.Command;
-
-namespace PosTech.Contacts.ApplicationCore.Messaging
+﻿namespace PosTech.Contacts.ApplicationCore.Messaging
 {
     public class ContactUpdatedMessage
     {
@@ -13,7 +11,7 @@ namespace PosTech.Contacts.ApplicationCore.Messaging
         public string DddState { get; set; }
         public string RegionName { get; set; }
 
-        public static explicit operator ContactUpdatedMessage(Contact contact)
+        public static explicit operator ContactUpdatedMessage(Entities.Command.Contact contact)
         {
             return new ContactUpdatedMessage
             {
@@ -25,6 +23,20 @@ namespace PosTech.Contacts.ApplicationCore.Messaging
                 DddCode = contact.Ddd.DddCode,
                 DddState = contact.Ddd.State,
                 RegionName = contact.Ddd.Region.RegionName,
+            };
+        }
+
+        public static explicit operator Entities.Query.Contact(ContactUpdatedMessage contactUpdatedMessage)
+        {
+            return new Entities.Query.Contact(contactUpdatedMessage.ContactId)
+            {
+                DddCode = contactUpdatedMessage.DddCode,
+                DddState = contactUpdatedMessage.DddState,
+                Email = contactUpdatedMessage.ContactEmail,
+                Name = contactUpdatedMessage.ContactName,
+                Phone = contactUpdatedMessage.ContactPhone,
+                RegionName = contactUpdatedMessage.RegionName,
+                Surname = contactUpdatedMessage.ContactSurname,
             };
         }
     }
---
+++ src/PosTech.Contacts.Worker/Consumers/ContactUpdatedMessageConsumer.cs
@@ -0,0 +1,41 @@
+﻿using PosTech.Contacts.ApplicationCore.Entities.Query;
+using PosTech.Contacts.ApplicationCore.Messaging;
+using PosTech.Contacts.ApplicationCore.Repositories.Query;
+using PosTech.Contacts.ApplicationCore.Serialization;
+using PosTech.Contacts.Infrastructure.Settings;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace PosTech.Contacts.Worker.Consumers
+{
+    public class ContactUpdatedMessageConsumer(IContactRepository contactRepository, RabbitMqSettings settings)
+        : RabbitMqConsumer(settings)
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await ConnectAsync(QueueNames.ContactUpdated, stoppingToken);
+
+            var consumer = new AsyncEventingBasicConsumer(Channel);
+            consumer.ReceivedAsync += ProcessMessageAsync;
+
+            await Channel.BasicConsumeAsync(QueueNames.ContactUpdated, autoAck: true, consumer: consumer, cancellationToken: stoppingToken);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine(
+                   $"Worker ativo em: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            }
+        }
+
+        protected override async Task ProcessMessageAsync(object sender, BasicDeliverEventArgs ea)
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var deserializedMessage = JsonSerializerHelper.Deserialize<ContactUpdatedMessage>(message);
+            var contact = (Contact)deserializedMessage;
+
+            await contactRepository.UpdateAsync(contact);
+        }
+    }
+}
