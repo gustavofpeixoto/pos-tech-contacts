a12aa7b3723d5c6f21e17691198b51b08f2f0446
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-05-25 17:54:43 -0300

    Alterar tipo de projeto do worker, ajustar o docker-compose e adicionar job para o worker no prometheus

--- src/PosTech.Contacts.Worker/Program.cs
+++ src/PosTech.Contacts.Worker/Program.cs
@@ -1,35 +1,54 @@
-using PosTech.Contacts.ApplicationCore.Repositories.Query;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using PosTech.Contacts.ApplicationCore;
 using PosTech.Contacts.Infrastructure;
-using PosTech.Contacts.Infrastructure.Settings;
-using PosTech.Contacts.Worker.Consumers;
+using PosTech.Contacts.Worker.Settings;
+using Prometheus;
+using Prometheus.DotNetRuntime;
+using Serilog;
+using System.Text.Json.Serialization;
 
-var builder = Host.CreateApplicationBuilder(args);
-
-builder.Configuration.AddJsonFileByName("sharedsettings");
-builder.Services.AddInfrastructureServices(builder.Configuration);
-
-var rabbitMqSettings = builder.Configuration.GetSection("RabbitMq").Get<RabbitMqSettings>();
-
-builder.Services.AddHostedService(sp =>
+internal class Program
 {
-    var contactRepository = sp.GetRequiredService<IContactRepository>();
-    return new ContactCreatedMessageConsumer(contactRepository, rabbitMqSettings);
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHostedService(sp =>
-{
-    var contactRepository = sp.GetRequiredService<IContactRepository>();
-    return new ContactUpdatedMessageConsumer(contactRepository, rabbitMqSettings);
-});
+        builder.Configuration.AddJsonFileByName("sharedsettings");
 
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-builder.Services.AddHostedService(sp =>
-{
-    var contactRepository = sp.GetRequiredService<IContactRepository>();
-    return new ContactDeletedMessageConsumer(contactRepository, rabbitMqSettings);
-});
+        var prometheusBuilder = DotNetRuntimeStatsBuilder.Default();
+        prometheusBuilder = DotNetRuntimeStatsBuilder.Customize()
+            .WithContentionStats(CaptureLevel.Informational)
+            .WithGcStats(CaptureLevel.Verbose)
+            .WithThreadPoolStats(CaptureLevel.Informational)
+            .WithExceptionStats(CaptureLevel.Errors)
+            .WithJitStats();
 
+        prometheusBuilder.RecycleCollectorsEvery(new TimeSpan(0, 20, 0));
+        prometheusBuilder.StartCollecting();
 
-var host = builder.Build();
+        builder.Services
+            .AddControllers(options
+                 => options.Conventions.Add(new RouteTokenTransformerConvention(new OutboundParameterTransformerSetting())))
+            .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+        builder.Services.AddSerilog();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddInfrastructureServices(builder.Configuration);
+        builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+        builder.Services.AddApplicationCoreServices();
+        builder.Services.AddEndpointsApiExplorer();
 
-host.Run();
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.UseHttpMetrics();
+        app.UseMetricServer();
+        app.MapControllers();
+        app.Run();
+    }
+}
---
+++ src/PosTech.Contacts.Worker/Settings/OutboundParameterTransformerSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PosTech.Contacts.Worker.Settings
+{
+    public partial class OutboundParameterTransformerSetting : IOutboundParameterTransformer
+    {
+        public string TransformOutbound(object value)
+        {
+            if (value == null) return null;
+
+            return ControllerNameRegex().Replace(value.ToString(), "$1-$2").ToLower();
+        }
+
+        [GeneratedRegex("([a-z])([A-Z])")]
+        private static partial Regex ControllerNameRegex();
+    }
+}
